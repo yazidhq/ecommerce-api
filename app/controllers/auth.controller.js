5cbe04f016fd21561ef21ecf8e0148b79ced44fb
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2024-11-06 13:24:50 +0700

    update: jwt header

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -5,7 +5,13 @@ const catchAsync = require("../utils/catch.async.");
 const AppError = require("../utils/app.error");
 
 const generateToken = (payload) => {
+  const header = {
+    alg: process.env.JWT_ALGORITHM,
+    typ: process.env.JWT_TYPE,
+  };
+
   return jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+    header: header,
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
@@ -36,6 +42,7 @@ const signUp = catchAsync(async (req, res, next) => {
 
   result.token = generateToken({
     id: result.id,
+    userType: result.userType,
   });
 
   return res.status(201).json({
@@ -59,6 +66,7 @@ const signIn = catchAsync(async (req, res, next) => {
 
   const token = generateToken({
     id: result.id,
+    userType: result.userType,
   });
 
   return res.status(200).json({
