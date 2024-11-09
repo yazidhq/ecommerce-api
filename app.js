34a5de294cf2887e8b17ef3111c6235b2dbef35b
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2024-11-10 01:48:19 +0700

    set static file

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(express.static("public"));
 
 const { glob } = require("glob");
 const path = require("path");
